843e60e6468a2a2d48ae5c3fab9e64da1153c7c6
tawashen <tawashen@gmail.com>
2025-07-06 16:12:44 +0900

    やっぱりTUIで行く

--- main.go
+++ main.go
@@ -71,12 +71,14 @@ type Outcome struct {
 	NextNodeID string `toml:"next_node_id"`
 }
 
+/*
  type PlayerStats {
 	HP int
 	CS int
 	GOLD int
-	
+
  }
+*/
 
 func init() {
 	fmt.Println("Initializing CRT...")
