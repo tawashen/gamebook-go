e0c912889ef1a905ecfaa407be696dc9e04b13c8
tawashen <tawashen@gmail.com>
2025-07-19 11:26:39 +0900

    interface実装開始

---
+++ LwSystem.go
@@ -0,0 +1,37 @@
+package lonewolf
+
+import (
+	"math/rand"
+	"time"
+)
+
+// KeyPair は戦闘結果テーブルのキーを定義
+type KeyPair struct {
+	RandNum  int `toml:"RandNum"`
+	ComRatio int `toml:"ComRatio"`
+}
+
+// DamagePair は戦闘結果テーブルの値を定義
+type DamagePair struct {
+	EnemyLoss  int  `toml:"EnemyLoss"`
+	PlayerLoss int  `toml:"PlayerLoss"`
+	IsKilled   bool `toml:"IsKilled"`
+}
+
+// LoneWolfSystem はLone Wolfゲームブックのルールを実装
+type LoneWolfSystem struct {
+	CRT       map[KeyPair]DamagePair // 戦闘結果テーブル
+	Rand      *rand.Rand             // 乱数生成器
+	CRTFile   string                 // 戦闘結果テーブルのファイルパス
+	ConfigDir string                 // 追加の設定ファイルディレクトリ（必要に応じて）
+}
+
+// NewLoneWolfSystem は新しいLoneWolfSystemインスタンスを生成
+func NewLoneWolfSystem(crtFile string) *LoneWolfSystem {
+	return &LoneWolfSystem{
+		CRT:       make(map[KeyPair]DamagePair),
+		Rand:      rand.New(rand.NewSource(time.Now().UnixNano())),
+		CRTFile:   crtFile,
+		ConfigDir: ".", // デフォルトはカレントディレクトリ
+	}
+}
